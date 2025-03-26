cbef40bd75859981cfbc64ed0244a1ebe47a3864
Makhmudbek Rasulov <mrslygrin@gmail.com>
2025-03-26 14:41:00 +0500

    [TICKET-2] feat: add axios instance setup

---
+++ src/api/httpClient.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+import { ROUTES } from '../shared/constants/routes.constants';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error('Unauthorized! Redirecting to login...');
+      window.location.href = ROUTES.auth.login;
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;

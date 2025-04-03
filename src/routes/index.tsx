93aafe94327bf577371b4918ec91da7ffca30d64
Akilbek Urinbaev <akilbekurinbaev0616@gmail.com>
2025-04-03 20:55:05 +0500

    [TICKET-18] feat: added for routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router';
 
 import { LoginPage } from '#/modules/auth';
+import { RentalPlanList } from '#/modules/rental-plan-details.tsx/rental-plan-list';
 import { ROUTES } from '#/shared/constants/routes.constants';
 
 import App from '../App';
 import { AddressDetails as AddressDetailsPage } from '../modules/address/address-details';
-import { PropertyDetails as PropertyDetailsPage } from '../modules/properties/property-details';
 import { CompaniesPage } from '../modules/companies';
+import { PropertyDetails as PropertyDetailsPage } from '../modules/properties/property-details';
 import { UnitDetailsPage } from '../modules/units/unit-details';
 import { Layout } from '../shared/layouts/dashboard';
 
@@ -37,6 +38,10 @@ export const router = createBrowserRouter([
           {
             path: '/addresses/details/:id',
             Component: AddressDetailsPage,
+          },
+          {
+            path: 'rental-plan/list',
+            Component: RentalPlanList,
           },
         ],
       },
